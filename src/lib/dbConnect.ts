12156d69bc6c4bbe8bba0ffd066c5aab9bba4857
princegupta-in <zprincegupta@gmail.com>
2025-01-29 18:53:53 +0530

    nextjs hotrelod issue with mongoose fixed

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,31 +1,73 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-type ConnectionObject = {
-    isConnected?: number //? is cuz of empty object
+// type ConnectionObject = {
+//     isConnected?: number //? is cuz of empty object
+// }
+
+// const connection: ConnectionObject = {}
+
+// async function dbConnect(): Promise<void> {
+//     if (connection.isConnected) {
+//         console.log("connecion to database already exist")
+//         // console.log(connection)// {isConnected: 1}
+//         return;
+//     }
+//     try {
+//         // const db = await mongoose.connect("mongodb+srv://princetsx:vO802VK4zLUt28SQ@cluster0.eden5.mongodb.net/")
+//         const db = await mongoose.connect(process.env.MONGO_URL!)
+
+//         // console.log("💲inside /lib/dbConnect.ts",db)
+//         connection.isConnected = db.connections[0].readyState
+
+//         console.log("db connected succesfully")
+//     } catch (error) {
+//         console.log("database connection failed⌛⌛", error)
+
+//         //failed connection,hence exit the process
+//         process.exit(1)
+//     }
+
+// }
+// export default dbConnect;
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log("🚩", MONGODB_URI)
+
+if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is not find in .env file");
 }
 
-const connection: ConnectionObject = {}
 
-async function dbConnect(): Promise<void> {
-    if (connection.isConnected) {
-        console.log("connecion to database already exist")
-        // console.log(connection)// {isConnected: 1}
-        return;
+//checking if already have connection
+//@ts-ignore
+let cached = global.mongoose;
+
+if (!cached) {
+    //@ts-ignore
+    cached = global.mongoose = { con: null, promise: null };
+}
+
+const dbConnect = async () => {
+    if (cached.con) {
+        return cached.con;
     }
+    if (!cached.promise) {
+
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10,
+        }
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => mongoose.connection);
+    }
+
     try {
-        // const db = await mongoose.connect("mongodb+srv://princetsx:vO802VK4zLUt28SQ@cluster0.eden5.mongodb.net/")
-        const db = await mongoose.connect(process.env.MONGO_URL!)
-
-        // console.log("💲inside /lib/dbConnect.ts",db)
-        connection.isConnected = db.connections[0].readyState
-
-        console.log("db connected succesfully")
+        cached.con = await cached.promise;
     } catch (error) {
-        console.log("database connection failed⌛⌛", error)
-
-        //failed connection,hence exit the process
-        process.exit(1)
+        cached.promise = null;
     }
 
+    return cached.con;
 }
 export default dbConnect;
